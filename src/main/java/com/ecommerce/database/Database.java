9215d18c70ec9eec8e4671ad49282c7487927bb1
truonghoangthuan <tthun32@gmail.com>
2021-12-02 17:39:46 +0700

    02/12/2021 17h40

--- src/main/java/com/ecommerce/database/Database.java
+++ src/main/java/com/ecommerce/database/Database.java
@@ -7,7 +7,7 @@ public class Database {
     public Connection getConnection() {
         Connection conn;
         try {
-            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/jsp-servlet-ecommerce-website", "root", "0974667060Th");
+            conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/jsp-servlet-ecommerce-website", "root", "root");
             return conn;
         } catch (Exception e) {
             System.out.println(e.getMessage());
